4f4325863b6c2df81a2a06367a432b8e70fe6314
jiao-shi-yu <40909521+jiao-shi-yu@users.noreply.github.com>
2020-06-16 19:38:00 +0800

    regx done

--- Java SE/regx/ReplaceAllDemo.java
+++ Java SE/regx/ReplaceAllDemo.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class ReplaceAllDemo {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		System.out.println("请输入:");
+		String line = scan.nextLine();
+		line = line.replaceAll("我[去操草日]", "**");
+		line = line.replaceAll("你[妈]", "**");
+		System.out.println(line);
 	}
 }
