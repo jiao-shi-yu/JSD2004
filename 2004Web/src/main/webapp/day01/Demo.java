23dd58b7adaf52df1e224d4355d2a1b349f65213
jiao-shi-yu <40909521+jiao-shi-yu@users.noreply.github.com>
2020-07-01 20:19:57 +0800

    HTML CSS

---
+++ 2004Web/src/main/webapp/day01/Demo.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Demo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        if (age>20) {
+
+        }
+        if (age>20) {
+
+        }
+        int[] ints = {1, 2, 3, 4};
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+        if (age>20) {
+
+        } else if {
+
+        }
+        
+        
+    }
+}
