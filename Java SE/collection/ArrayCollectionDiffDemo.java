7f162ae3243a755100a5e9353d339985f6eed5f7
jiao-shi-yu <40909521+jiao-shi-yu@users.noreply.github.com>
2020-07-03 09:19:07 +0800

    asListDemo

---
+++ Java SE/collection/ArrayCollectionDiffDemo.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class ArrayCollectionDiffDemo {
+	public static void main(String[] args) {
+		String[] strings = {"one", "two"};
+		Collection<String> stringCollection = Arrays.asList(strings);
+	}
+}
