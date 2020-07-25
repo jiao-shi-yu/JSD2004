420a2136a0700fd12ede652dbdfd1b42c23ecf67
jiao-shi-yu <40909521+jiao-shi-yu@users.noreply.github.com>
2020-07-25 18:13:26 +0800

    servlet 3.0 multipart upload fileName
    String header = part.getHeader("Content-Disposition");
    		System.out.println("\n\t" + header);
    
    		String fileName = header.substring(header.indexOf("filename=\"") + 10, header.lastIndexOf("\""));
    		System.out.println("[fileName]: " + fileName);
    
    		fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
    		System.out.println("[fileName]: " + fileName);
    		return fileName;

--- servlet_4_1/src/main/java/com/jiaoshy/controller/UploadServlet.java
+++ servlet_4_1/src/main/java/com/jiaoshy/controller/UploadServlet.java
@@ -1,9 +1,11 @@
 package com.jiaoshy.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,20 +14,38 @@ import javax.servlet.http.Part;
 /**
  * Servlet implementation class UploadServlet
  */
-@MultipartConfig
+@MultipartConfig // 上传文件
+@WebServlet(name = "UploadServlet", urlPatterns = { "/upload.do" })
 public class UploadServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-  
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 由于是post请求，request设置字符集
 		request.setCharacterEncoding("UTF-8");
-		// 获取上传文件的信息， pic为上传文件参数名
-		Part part = request.getPart("pic");
-		String fileName = part.getSubmittedFileName();
-		part.write("D:/"+fileName);
-		System.out.println("上传完成");
+		// 获取上传的文件对象的信息， pic为上传文件参数名
+		Part part = request.getPart("picture");
+
+		String fileName = getFileName(part);
+
+		part.write("C:/apache-tomcat-8.5.45/webapps/imgs/"+fileName);
+
+		/// -----> 响应部分
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("OK!<br><img src='http://localhost:8080/imgs/"+fileName+"'>");
+
+	}
+
+	private String getFileName(Part part) {
+		String header = part.getHeader("Content-Disposition");
+		System.out.println("\n\t" + header);
+
+		String fileName = header.substring(header.indexOf("filename=\"") + 10, header.lastIndexOf("\""));
+		System.out.println("[fileName]: " + fileName);
+
+		fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
+		System.out.println("[fileName]: " + fileName);
+		return fileName;
 	}
 
 }
